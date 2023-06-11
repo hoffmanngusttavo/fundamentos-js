0d27adce28f0676f698215e09702ef97941cdd80
Gustavo H <hoffmann.gusttavo@gmail.com>
2023-06-11 19:53:14 -0300

    ordenação

--- ordenacao/aula-1/insertionSort.js
+++ ordenacao/aula-1/insertionSort.js
@@ -4,9 +4,9 @@ const troca = require('./troca');
 
 function insertionSort(lista) {
 
-  for (let atual = 0; atual < lista.length; atual++) {
+  for (let atual = 1; atual < lista.length; atual++) {
     let analise = atual;
-    while (analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
+    while (lista[analise].preco < lista[analise - 1].preco) {
       troca(lista, analise);
       analise--
     }
